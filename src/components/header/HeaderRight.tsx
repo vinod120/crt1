c537d534e643d6811517588c55f2c3662bb291e4
vinod120 <vinodgate2016@gmail.com>
2025-07-10 01:10:59 +0530

    changes

--- src/components/header/HeaderRight.tsx
+++ src/components/header/HeaderRight.tsx
@@ -37,7 +37,6 @@ const HeaderRight: React.FC = () => {
             tabIndex={0}
             aria-label="Toggle Theme"
             onClick={() => dispatch(toggleTheme())}
-            className="crt-header-icon"
           >
             {themeType === "light" ? <SunOutlined /> : <MoonOutlined />}
           </span>
