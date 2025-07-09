bce61ee0ad61e39de46e71a9c3eb9e68e3d35626
vinod120 <vinodgate2016@gmail.com>
2025-07-09 22:57:41 +0530

    changes

--- src/pages/dashboard/DashboardCards.tsx
+++ src/pages/dashboard/DashboardCards.tsx
@@ -2,14 +2,34 @@ import { formatNumber } from "@/utils";
 import { AppstoreOutlined, ContainerOutlined } from "@ant-design/icons";
 import { Card, Col, Row, Skeleton } from "antd";
 import type { FC } from "react";
-import './DashboardCards.css';
+import "./DashboardCards.css";
 import { DashboardCardsProps } from "./types";
 
 const defaultCards = [
-  { id: 1, title: "Assets", key: "total_assets", icon: <AppstoreOutlined style={{ fontSize: '20px' }} /> },
-  { id: 2, title: "Study Setup", key: "total_study_setups", icon: <ContainerOutlined style={{ fontSize: '20px' }} /> },
-  { id: 3, title: "Study", key: "study_count", icon: <AppstoreOutlined style={{ fontSize: '20px' }} /> },
-  { id: 4, title: "Reports", key: "report_count", icon: <AppstoreOutlined style={{ fontSize: '20px' }} /> },
+  {
+    id: 1,
+    title: "Assets",
+    key: "total_assets",
+    icon: <AppstoreOutlined style={{ fontSize: "20px" }} />,
+  },
+  {
+    id: 2,
+    title: "Study Setup",
+    key: "total_study_setups",
+    icon: <ContainerOutlined style={{ fontSize: "20px" }} />,
+  },
+  {
+    id: 3,
+    title: "Study",
+    key: "study_count",
+    icon: <AppstoreOutlined style={{ fontSize: "20px" }} />,
+  },
+  {
+    id: 4,
+    title: "Reports",
+    key: "report_count",
+    icon: <AppstoreOutlined style={{ fontSize: "20px" }} />,
+  },
 ];
 
 const DashboardCards: FC<DashboardCardsProps> = ({
@@ -19,36 +39,37 @@ const DashboardCards: FC<DashboardCardsProps> = ({
   setSelectedCard,
 }) => {
   const getCardCount = (key: string) => formatNumber(counts?.[key] ?? 0);
-
+  const renderSkeleton = () =>
+    defaultCards?.map((card, idx) => (
+      <Col key={card.id || idx} xs={24} sm={12} lg={6}>
+        <Card className="dashboard-card-container">
+          <Skeleton.Input active size="small" block />
+        </Card>
+      </Col>
+    ));
+  const renderCards = () => {
+    return defaultCards.map((card, idx) => (
+      <Col key={card.id || idx} xs={24} sm={12} lg={6}>
+        <Card
+          className="dashboard-card-container"
+          // onClick={() => setSelectedCard?.(card.key)}
+        >
+          <div className="dashboard-card-content">
+            <div className="dashboard-card-left">
+              {card.icon}
+              <span className="dashboard-card-title">{card.title}</span>
+            </div>
+            <div className="dashboard-card-count">{getCardCount(card.key)}</div>
+          </div>
+        </Card>
+      </Col>
+    ));
+  };
   return (
     <Row gutter={[16, 16]}>
-      {countsLoading
-        ? defaultCards.map((card, idx) => (
-            <Col key={card.id || idx} xs={24} sm={12} lg={6}>
-              <Card className="dashboard-card-container">
-                <Skeleton.Input active size="small" block />
-              </Card>
-            </Col>
-          ))
-        : defaultCards.map((card, idx) => (
-            <Col key={card.id || idx} xs={24} sm={12} lg={6}>
-              <Card
-                className="dashboard-card-container"
-                // onClick={() => setSelectedCard?.(card.key)}
-              >
-                <div className="dashboard-card-content">
-                  <div className="dashboard-card-left">
-                    {card.icon}
-                    <span className="dashboard-card-title">{card.title}</span>
-                  </div>
-                  <div className="dashboard-card-count">{getCardCount(card.key)}</div>
-                </div>
-              </Card>
-            </Col>
-          ))}
+      {countsLoading ? renderSkeleton() : renderCards()}
     </Row>
   );
 };
 
 export default DashboardCards;
-
