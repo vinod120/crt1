2413b604729dd8641a61b3a9fd665a1b6ea992cc
vinod120 <vinodgate2016@gmail.com>
2025-07-10 16:25:11 +0530

    sidebar css separated theme wise

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import { ConfigProvider, theme } from 'antd';
-import { useSelector } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { ConfigProvider, theme } from "antd";
+import { useSelector } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 import AppLayout from "./layout/appLayout";
-import { RootState } from './store';
+import { RootState } from "./store";
 
 const App = () => {
   const themeType = useSelector((state: RootState) => state.theme.theme);
   return (
-      <ConfigProvider theme={{ algorithm: themeType === 'light' ? theme.defaultAlgorithm : theme.darkAlgorithm }}>
-        <div className="app-container">
+    <ConfigProvider
+      theme={{
+        algorithm:
+          themeType === "light" ? theme.defaultAlgorithm : theme.darkAlgorithm,
+      }}
+    >
+      <div className="app-container">
         <Router>
           <AppLayout />
         </Router>
-        </div>
-      </ConfigProvider>
+      </div>
+    </ConfigProvider>
   );
 };
 
